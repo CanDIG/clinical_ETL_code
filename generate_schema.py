0e80677ce1718580a5d9783f2133c16b18f650c9
Karen Cranston <karen.cranston@gmail.com>
2022-11-01 08:04:43 -0400

    removing katsu schema generation from csvconvert

--- generate_schema.py
+++ generate_schema.py
@@ -13,12 +13,16 @@ import sys
 import yaml
 
 import argparse
+from chord_metadata_service.mcode.schemas import MCODE_SCHEMA
+from schemas import candigv1_schema
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', type=str, help="Path to either an xlsx file or a directory of csv files for ingest")
     # parser.add_argument('--api_key', type=str, help="BioPortal API key found in BioPortal personal account settings")
     # parser.add_argument('--email', type=str, help="Contact email to access NCBI clinvar API. Required by Entrez")
+    parser.add_argument('--template', type=str, help="If provided, generate a mapping template at the specified file")
     parser.add_argument('--schema', type=str, help="Schema to use for template; default is mCodePacket")
     parser.add_argument('--mapping', '--manifest', type=str, help="Path to a manifest file describing the mapping."
                                                                   " See README for more information")
@@ -188,6 +192,68 @@ def ingest_raw_data(input_path, indexed):
             raw_csv_dfs[df].reset_index(inplace=True)
     return raw_csv_dfs, output_file
 
+
+def generate_mapping_template(node, node_name="", node_names=None):
+    """Create a template for mcodepacket, for use with the --template flag."""
+    if node_names is None:
+        node_names = []
+    if node_name != "":
+        # check to see if the last node_name is a header for this node_name:
+        if len(node_names) > 0:
+            x = node_names.pop()
+            x_match = re.match(r"(.+?)\**,.*", x)
+            if x_match is not None:
+                if x_match.group(1) in node_name:
+                    node_names.append(f"##{x}")
+                else:
+                    node_names.append(x)
+            else:
+                node_names.append(x)
+        if "description" in node:
+            node_names.append(f"{node_name},\"##{node['description']}\"")
+        else:
+            node_names.append(f"{node_name},")
+    if "type" in node:
+        if node["type"] == "string":
+            return "string", node_names
+        elif node["type"] == "array":
+            new_node_name = ".".join((node_name, "0"))
+            sc, nn = generate_mapping_template(node["items"], new_node_name, node_names)
+            return [sc], nn
+        elif node["type"] in ["number", "integer"]:
+            return 0, node_names
+        elif node["type"] == "boolean":
+            return True, node_names
+        elif node["type"] == "object":
+            scaffold = {}
+            if "$id" in node:
+                scaffold["$id"] = node["$id"]
+            if len(node_names) > 0:
+                # if this is an ontology_class_schema, we'll update this data post-mapping
+                if "$id" in node and (node["$id"] == "katsu:common:ontology_class"
+                                      or node["$id"] == "katsu:mcode:complex_ontology"):
+                    # add a + to the name of the node to denote that this needs to be looked up in an ontology
+                    name = node_names.pop()
+                    name_match = re.match(r"(.+?),(.+)", name)
+                    if name_match is not None:
+                        name = f"{name_match.group(1)}+,{name_match.group(2)}"
+                    node_names.append(name)
+                    return node["$id"], node_names
+            if "properties" in node:
+                for prop in node["properties"]:
+                    if node_name == "":
+                        new_node_name = prop
+                    else:
+                        new_node_name = ".".join((node_name, prop))
+                    if "required" in node and prop in node["required"]:
+                        new_node_name += "*"
+                    scaffold[prop], node_names = generate_mapping_template(node["properties"][prop], new_node_name, node_names)
+            return scaffold, node_names
+    else:
+        return {}, node_names
+    return None, node_names
+
+
 def process_mapping(line, test=False):
     """Given a csv mapping line, process into its component pieces."""
     line_match = re.match(r"(.+?),(.*$)", line.replace("\"", ""))
@@ -308,12 +374,34 @@ def main(args):
     # api_key = args.api_key
     input_path = args.input
     # email = args.email
-    #template = args.template
+    template = args.template
     mapping = args.mapping
     schema = args.schema
     mappings.VERBOSE = args.verbose
     metadata = ""
     
+    # if template is provided, we should generate a template file
+    if template is not None:
+        if schema is None:
+            schema = MCODE_SCHEMA
+            # get metadata about version of MCODE_SCHEMA used:
+            metadata += "## schema based on version " + version('katsu') + ",\n"
+            direct_url = [p for p in files('katsu') if 'direct_url.json' in str(p)]
+            if len(direct_url) > 0:
+                d = json.loads(direct_url[0].read_text())
+                metadata += f"## directly checked out from {d['url']}, commit {d['vcs_info']['commit_id']}\n"
+        if schema == "candigv1":
+            schema = candigv1_schema
+        sc, node_names = generate_mapping_template(schema)
+            
+        with open(f"{template}.csv", 'w') as f:  # write to csv file for mapping
+            f.write(metadata)
+            f.write("## mcodepacket element, description (overwrite with mapped element)\n")
+            f.write("## (.0 is an array element) (* is required) (+ denotes ontology term),\n")
+            for nn in node_names:
+                f.write(f"{nn}\n")
+        return
+
     # if mapping is provided, we should create a mapping scaffold
     if mapping is not None:
         manifest = load_manifest(mapping)
@@ -365,3 +453,4 @@ def main(args):
 
 if __name__ == '__main__':
     main(parse_args())
+

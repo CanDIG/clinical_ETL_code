2eb0b9dfcf99f40d7bec0e67e7612e4457711967
Karen Cranston <karen.cranston@gmail.com>
2023-03-10 14:34:19 -0500

    reuse skip schema list

--- moh_mappings.py
+++ moh_mappings.py
@@ -16,8 +16,11 @@ The top-level keys of the schema are:
 
 class mohschema:
     schema = {} 
+    skipped_schemas = [
+        "Patched","Paginated","Discovery","Request","data_ingest","moh","Enum"
+        ]
 
-    def __init__(self,url):
+    def __init__(self,url, simple=False):
         """Retrieve the schema from the supplied URL, return as dictionary."""
         resp = requests.get(url)
         schema = yaml.safe_load(resp.text)
@@ -30,31 +33,40 @@ class mohschema:
         self.schema = schema
 
     def generate_schema_array(self):
-        components = self.schema["components"]["schemas"]
+        """Generates an array of schema objects and their properties that can
+        then be export into a mapping template file. Same code as generate_scaffold
+        but returns an array instead of a dict. """
         schema_array = []
-        for component in components.keys():
+        schema_objects = self.schema["components"]["schemas"]
+        for s in schema_objects.keys():
         # each of these is potentially an MoH schema, e.g. Donor, or Treatment
         # skip the ones that are internal to katsu
-            if re.match("Patched",component):
-                continue 
-            if re.match("Paginated",component):
-                continue 
-            if re.match("Discovery",component):
-                continue 
-            if re.search("Request",component):
-                continue 
-            if re.match("data_ingest",component):
-                continue 
-            if re.match("moh",component):
-                continue 
-            if re.search("Enum", component):
-                continue
-            schema_array.append(f"# Schema {component}\n")
-            properties = components[component]["properties"]
-            for k in properties.keys():
-                # format of each line is : schema.field, # help text 
-                schema_array.append(f"{component}.{k}, ## add mapping function here\n")
+            for test in self.skipped_schemas:
+                if re.search(test,s):
+                    break
+            else: 
+                schema_array.append(f"# Schema {s}\n")
+                properties = schema_objects[s]["properties"]
+                for k in properties.keys():
+                    # format of each line is : schema.field, # help text 
+                    schema_array.append(f"{s}.{k}, ## add mapping function here\n")
         return schema_array
+
+    def generate_scaffold(self):
+        """Generate a simplied version of the schema that can be used by CSVConvert 
+        as the internal scaffold when parsing data. Similar code as 
+        generate_schema_array but returns a dict instead of an array."""
+        scaffold = {}
+        schema_objects = self.schema["components"]["schemas"]
+        for s in schema_objects.keys():
+            for test in self.skipped_schemas:
+                if re.search(test,s):
+                    break
+            else:
+                #properties = schema_objects[s]["properties"]
+                #scaffold[s] = properties
+                scaffold[s] = schema_objects[s]
+        return scaffold
 
     def get_schema_data(self, schema_name):
         """Given the name of a schema object, return the type"""
@@ -62,12 +74,20 @@ class mohschema:
             return self.schema["components"]["schemas"][schema_name]
         except KeyError:
             raise KeyError(f"No schema oject {schema_name} in schema")
-            
 
     def get_schema_type(self, schema_name):
         """Given the name of a schema object, return the type"""
-        schema_data = self.get_schema_data(schema_name)
-        return schema_data['type']
+
+        # next two code lines are how this should work, once we have the info in
+        # the schema, although might need modification depending on final structure
+        #schema_data = self.get_schema_data(schema_name)
+        #return schema_data['type']
+
+        # but in the meantime, as a hack, we hard code everything 
+        if schema_name == "Donor":
+            return "not_an_array"
+        else:
+            return "array"
 
     def get_property_type(self,schema_name,node):
         """Given the name of a schema object, and a property of that node, 

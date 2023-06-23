197fd3530e5299d1a65ea673902114c77e8b0021
Daisie Huang <daisieh@gmail.com>
2023-06-22 18:17:55 -0700

    update tests

--- test_data_ingest.py
+++ test_data_ingest.py
@@ -1,33 +1,42 @@
 import CSVConvert
+import mappings
 
 # read sheet from given data pathway
 raw_csvs, output_file = CSVConvert.ingest_raw_data("test_data/pytest_data_v2.xlsx", [])
-indexed_data = CSVConvert.process_data(raw_csvs, "Subject")
+mappings.INDEXED_DATA = CSVConvert.process_data(raw_csvs, "Subject")
+mappings.push_to_stack(None, None, mappings.IDENTIFIER)
 
 def test_single_val():
-    test = CSVConvert.eval_mapping("ABC-01-03", None, indexed_data, "{single_val(THER_TX_NAME)}", None)
+    mappings.IDENTIFIER = "ABC-01-03"
+    test = CSVConvert.eval_mapping(mappings.IDENTIFIER, "{single_val(THER_TX_NAME)}", None, None)
     assert test == "IRINOTECAN,IRINOTECAN"
 
 
 def test_date():
-    test = CSVConvert.eval_mapping("ABC-01-03", None, indexed_data, "{single_date(DTH_DT_RAW)}", None)
+    mappings.IDENTIFIER = "ABC-01-03"
+    test = CSVConvert.eval_mapping(mappings.IDENTIFIER, "{single_date(DTH_DT_RAW)}", None, None)
     assert test == "2023-09-25"
 
 
 def test_list_val():
-    test = CSVConvert.eval_mapping("ABC-01-05", None, indexed_data, "{list_val(DTH_DT_RAW)}", None)
+    mappings.IDENTIFIER = "ABC-01-05"
+    test = CSVConvert.eval_mapping(mappings.IDENTIFIER, "{list_val(DTH_DT_RAW)}", None, None)
     assert len(test) == 2
 
-    test = CSVConvert.eval_mapping("ABC-01-03", None, indexed_data, "{list_val(THER_TX_NAME)}", None)
+    mappings.IDENTIFIER = "ABC-01-03"
+    test = CSVConvert.eval_mapping(mappings.IDENTIFIER, "{list_val(THER_TX_NAME)}", None, None)
     assert test == ['IRINOTECAN,IRINOTECAN', 'IRINOTECAN,IRINOTECAN']
 
-    test = CSVConvert.eval_mapping("ABC-01-03", None, indexed_data, "{flat_list_val(THER_TX_NAME)}", None)
+    mappings.IDENTIFIER = "ABC-01-03"
+    test = CSVConvert.eval_mapping(mappings.IDENTIFIER, "{flat_list_val(THER_TX_NAME)}", None, None)
     assert test == ['IRINOTECAN', 'IRINOTECAN', 'IRINOTECAN', 'IRINOTECAN']
 
 
 def test_multiple_sheet_val():
-    test = CSVConvert.eval_mapping("ABC-01-04", None, indexed_data, "{single_val(Diagnosis.DTH_DT_RAW)}", None)
+    mappings.IDENTIFIER = "ABC-01-04"
+    test = CSVConvert.eval_mapping(mappings.IDENTIFIER, "{single_val(Diagnosis.DTH_DT_RAW)}", None, None)
     assert test == '2024 OCT 25'
 
-    test = CSVConvert.eval_mapping("ABC-01-04", None, indexed_data, "{list_val(DTH_DT_RAW)}", None)
+    mappings.IDENTIFIER = "ABC-01-04"
+    test = CSVConvert.eval_mapping(mappings.IDENTIFIER, "{list_val(DTH_DT_RAW)}", None, None)
     assert test == ['2024 OCT 25', '2023 SEP 25']

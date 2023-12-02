0769db0b6e9bf63c29f402755ca831c510c43ebc
Daisie Huang <daisieh@gmail.com>
2023-12-01 19:59:04 -0800

    temporary: new UUID katsu

--- update_moh_template.sh
+++ update_moh_template.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-python generate_schema.py --out tmp_template
+python generate_schema.py --url https://raw.githubusercontent.com/CanDIG/katsu/sonchau/uuid_with_fk/chord_metadata_service/mohpackets/docs/schema.yml --out tmp_template
 diff tmp_template.csv moh_template.csv > test_data/moh_diffs.txt
 bytes=$(head -5 test_data/moh_diffs.txt | wc -c)
 dd if=test_data/moh_diffs.txt  bs="$bytes" skip=1 conv=notrunc of=test_data/moh_diffs.txt

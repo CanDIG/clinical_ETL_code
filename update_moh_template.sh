4da62907d39729a2e2854cf9fcd3c8aa0f93faf6
David Brownlee <david.brownlee@computationalgenomics.ca>
2024-10-25 15:04:32 -0400

    github workflow corrected.

--- update_moh_template.sh
+++ update_moh_template.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+# Updates the moh_template based on the schema.
+# Manual differences are recorded in tests/moh_diffs.txt
 
-
-python src/clinical_etl/generate_schema.py --out tmp_template
-diff tmp_template.csv moh_v3_template.csv > tests/moh_diffs.txt
-rm tmp_template.csv
-bytes=$(head -5 tests/moh_diffs.txt | wc -c)
-dd if=tests/moh_diffs.txt  bs="$bytes" skip=1 conv=notrunc of=tests/moh_diffs1.txt
+python src/clinical_etl/generate_schema.py --out moh_template
+diff moh_template.csv moh_v3_template.csv > curr_diff.txt
+bytes=$(head -5 curr_diff.txt | wc -c)
+dd if=curr_diff.txt  bs="$bytes" skip=1 conv=notrunc of=tests/moh_diffs1.txt
 mv tests/moh_diffs1.txt tests/moh_diffs.txt
+rm curr_diff.txt

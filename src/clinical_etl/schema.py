ec37bb19f3e4e8665eac4fce039a7e0d249bcae3
Marion <mshadbolt@users.noreply.github.com>
2025-03-25 09:01:49 -0700

    fix bug where not available deleted

--- src/clinical_etl/schema.py
+++ src/clinical_etl/schema.py
@@ -410,13 +410,18 @@ class BaseSchema:
                     "missing": 0
                 }
             self.statistics["required_but_missing"][schema_name][f]["total"] += 1
-            if f not in map_json or map_json[f] == "Not available":
+            if f not in map_json:
                 self.warn(f"{f} required for {schema_name}")
                 self.statistics["required_but_missing"][schema_name][f]["missing"] += 1
                 if case not in self.statistics["cases_missing_data"]:
                     self.statistics["cases_missing_data"].append(case)
                 map_json[f] = None
                 remove_these.append(f)
+            elif map_json[f] == "Not available":
+                self.warn(f"{f} required for {schema_name}")
+                self.statistics["required_but_missing"][schema_name][f]["missing"] += 1
+                if case not in self.statistics["cases_missing_data"]:
+                    self.statistics["cases_missing_data"].append(case)
 
         eval(f"self.validate_{schema_name}({map_json})")
         for f in remove_these:

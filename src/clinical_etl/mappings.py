872d71c776f70fffbd38047cb91467da9c8f355c
Marion <mshadbolt@users.noreply.github.com>
2026-01-06 15:46:49 -0800

    add rounding to int method

--- src/clinical_etl/mappings.py
+++ src/clinical_etl/mappings.py
@@ -272,12 +272,15 @@ def int_to_date_interval_json(data_values):
 def set_neg_99_blank_int(data_values):
     """Sets to blank if -99 used to indicate a value is not available or returns input value"""
     val = single_val(data_values)
+    val = int(round(val))
     try:
-        if float(val) == -99:
+        if val == -99:
             return None
         else:
             return int(val)
     except TypeError:
+        return None
+    except ValueError:
         return None
 
 
